e701c7950b97d0aa3117b620104343be9c144079
Eric Fang <gzeureka@gmail.com>
2020-07-05 11:53:13 +0800

    Implement

--- src/consistent_hash/core.clj
+++ src/consistent_hash/core.clj
@@ -1,7 +1,37 @@
 (ns consistent-hash.core
-  (:gen-class))
+  (:require [clojure.math.numeric-tower :as math]
+            )
+  )
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def m (math/expt 2 32))
+
+(defn consistent-hash [x number-of-nodes]
+  (math/ceil (/ (mod x m) (int (/ m number-of-nodes)))))
+
+(defn average [num-seq]
+  (/ (apply + num-seq) (count num-seq)))
+
+(defn standard-deviation [num-seq]
+  (let [avg (average num-seq)]
+    (->> (map (fn [x]
+                (let [n (math/abs (- x avg))]
+                  (* n n)
+                  )
+                ) num-seq)
+         average
+         math/sqrt
+         )
+    )
+  )
+
+(defn -main [& args]
+  (let [number-of-nodes 10
+        times 1000000
+        hash-values  (repeatedly times (fn [] (consistent-hash (long (rand m)) number-of-nodes)))
+        loads (sort (reduce (fn [ret node] (update ret node inc)) (zipmap (range 1 (inc number-of-nodes)) (repeat 0)) hash-values))
+        ]
+    (println "Server load: " loads)
+    (println "Standard deviation: " (standard-deviation (map second loads)))
+    )
+  )
+
