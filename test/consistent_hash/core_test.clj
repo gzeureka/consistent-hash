c0fcb00708b492158124e02f7777538f39634960
Eric Fang <gzeureka@gmail.com>
2020-07-05 10:05:33 +0800

    Init commit

---
+++ test/consistent_hash/core_test.clj
@@ -0,0 +1,7 @@
+(ns consistent-hash.core-test
+  (:require [clojure.test :refer :all]
+            [consistent-hash.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
